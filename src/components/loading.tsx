1fc1edad383293a7db5caed59d0d6b6a4f21166a
renanconstancio <renanhconstancio@gmail.com>
2022-10-24 23:07:16 -0300

    ajusts

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,7 +1,11 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function Loading() {
   return (
     <div className="h-screen flex flex-1 flex-row justify-center items-center">
-      <i className="fas fa-spinner fa-pulse fa-3x"></i>
+      <FontAwesomeIcon icon={faSpinner} className="fa-pulse fa-3x" />
+      {/* <i className="fas fa-spinner fa-pulse fa-3x"></i> */}
     </div>
   );
 }
