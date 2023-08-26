1e3a9ad9d05b1713cf9918c035caefdfaa018c40
Renan Henrique Constancio <renanhconstancio@gmail.com>
2023-08-26 14:35:51 +0000

    feat: setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,41 @@ import "owl.carousel/dist/assets/owl.theme.default.css";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "./global/styles/app.css";
 
+import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
 
 import { AuthProvider } from "./context/auth";
 import { AlertProvider } from "./context/alert";
 import { ModalProvider } from "./context/modal";
 import RouteIndex from "./routes";
-import React from "react";
+
+// Create a client
+const queryClient = new QueryClient({
+  
+  defaultOptions: {
+    
+    queries: {
+      refetchOnWindowFocus: false,
+      cacheTime: 1000 * 30 * 60
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <AuthProvider>
-        <AlertProvider>
-          <ModalProvider>
-            <RouteIndex />
-          </ModalProvider>
-        </AlertProvider>
-      </AuthProvider>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <AuthProvider>
+          <AlertProvider>
+            <ModalProvider>
+              <RouteIndex />
+            </ModalProvider>
+          </AlertProvider>
+        </AuthProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
